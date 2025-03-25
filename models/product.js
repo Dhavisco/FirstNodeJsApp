d129fe552d7bc9c869212f2d95ee513772a23dce
Dhavisco <olaniyidavid2002@gmail.com>
2025-03-25 15:14:54 +0100

    sql

--- models/product.js
+++ models/product.js
@@ -1,37 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../util/database')
 
-const p =  path.join(path.dirname(process.mainModule.filename),
-        'data', 
-        'products.json'
-    );
+const Cart = require('./cart');
 
-const getProductFromFile = cb => {
-     
-        fs.readFile(p, (err, fileContent) => {
-            if (err){
-              return cb([]);
-            } else {
-                cb(JSON.parse(fileContent));
-            }
-            
-        })
-}
-module.exports = class Product{
-    constructor(t){
-        this.title = t;
-    }
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save(){
-      getProductFromFile(products => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-            console.log(err);
-        })
-      });
-}
+  save() {
+   return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES(?, ?, ?, ?)', 
+    [this.title, this.price, this.imageUrl, this.description]
+);
+  }
 
-    static fetchAll(cb){
-       getProductFromFile(cb)
-    }
-}
+  static deleteById(id) {
+    
+  }
+
+  static fetchAll() {
+  return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id) {
+   return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+};
+
+};
