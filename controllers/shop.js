6b41c4897431f0e5b6206cb5427819de494e5152
Dhavisco <olaniyidavid2002@gmail.com>
2025-04-10 18:37:58 +0100

    Adding Existing Products and Retrieving Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -110,6 +110,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
   .getCart()
   .then(cart => {
@@ -121,24 +122,26 @@ exports.postCart = (req, res, next) => {
     if (products.length > 0) {
       product = products[0];
     }
-    let newQuantity = 1;
-    if (product) {
     
+    if (product) {
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity + 1;
+      return product;
+     
     }
     return Product.findByPk(prodId)
-    .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: { quantity: newQuantity }
-      });
-    })
-    .catch(err => console.log(err));
-    
   })
-
-  // .then(() => {
-  //   res.redirect('/cart');
-  // })
+  .then(product =>{
+     return fetchedCart.addProduct(product, {
+      through: { quantity: newQuantity }
+    });
+  })
+  .then(() => {
+    res.redirect('/cart');
+  })
   .catch(err => console.log(err));
+
+
   // Product.findById(prodId, product => {
   //   Cart.addProduct(prodId, product.price);
   // });
