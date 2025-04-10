c733feee202286b026e2a1a75ca374b27d437996
Dhavisco <olaniyidavid2002@gmail.com>
2025-04-10 18:03:14 +0100

    creating and fetching a cart

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ sequelize.sync().then(result => {
   return user;
 }).then(user => {
   // console.log(user)
+  return user.createCart();
+}).then (cart => {
   app.listen(3000);
 })
 .catch(err => {
